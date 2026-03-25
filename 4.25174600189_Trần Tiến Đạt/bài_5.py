9064f567da4f0bfe1f2c3ea76a83e6729079ccab
TranTienDat-25174600189-19A4 <ttdat.25174600189@sv.uneti.edu.vn>
2026-03-25 12:32:23 +0700

    Delete 4.25174600189_Trần Tiến Đạt directory

--- 4.25174600189_Trần Tiến Đạt/bài_5.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Nhập số: "))
-
-dao = 0
-while n != 0:
-    du = n % 10
-    dao = dao * 10 + du
-    n = n // 10
-
-print("Số đảo:", dao)
--- 4.25174600189_Trần Tiến Đạt/bài_9.py
+++
@@ -1,9 +0,0 @@
-while True:
-    id = input("Nhập ID: ")
-    pw = input("Nhập password: ")
-
-    if id == "25174600189" and pw == "88888888":
-        print("Đăng nhập thành công")
-        break
-    else:
-        print("Sai, đăng nhập lại!")
